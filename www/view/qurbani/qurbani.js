10196d969610cb897da4d7f8e0253549cc5ba1a2
kashiffazal99 <kashiffazal99@gamil.com>
2015-09-20 09:47:12 +0500

    rt

--- www/view/qurbani/qurbani.js
+++ www/view/qurbani/qurbani.js
@@ -5,7 +5,49 @@
 
 angular.module('starter')
 
-.controller("qurbaniController",function($scope,dataService){
+.controller("qurbaniController",function($scope,dataService,$cordovaGeolocation){
+    //geoLocation code
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+    $cordovaGeolocation
+      .getCurrentPosition(posOptions)
+      .then(function (position) {
+        var lat  = position.coords.latitude
+        var long = position.coords.longitude
+      }, function(err) {
+        // error
+      });
+
+
+    var watchOptions = {
+      frequency : 1000,
+      timeout : 3000,
+      enableHighAccuracy: false // may cause errors if true
+    };
+
+    var watch = $cordovaGeolocation.watchPosition(watchOptions);
+    watch.then(
+      null,
+      function(err) {
+        // error
+      },
+      function(position) {
+        var lat  = position.coords.latitude
+        var long = position.coords.longitude
+      });
+
+
+    watch.clearWatch();
+    // OR
+    $cordovaGeolocation.clearWatch(watch)
+      .then(function(result) {
+        // success
+      }, function (error) {
+        // error
+      });
+
+    //geo location code end
+
+
     $scope.user = {};
     $scope.question1 = true;
     $scope.question2 = false;
